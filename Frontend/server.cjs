f287d5dedd6fac8fc5a9f19f1401e802e7fadb6b
Asadullah378 <asadnawaz378@gmail.com>
2025-11-16 04:48:39 +0200

    Update server.cjs

--- Frontend/server.cjs
+++ Frontend/server.cjs
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const PORT = 3000;
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || '127.0.0.1';
 const DIST_DIR = path.join(__dirname, 'dist');
 
 const MIME_TYPES = {
